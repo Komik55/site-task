5132a7df90e39122d636a7feec94752bb5785b21
Komik55 <komyobniyozivich@gmail.com>
2022-06-21 16:21:17 +0500

    added

--- js/script.js
+++ js/script.js
@@ -35,21 +35,22 @@ prices.forEach((elem) => {
     projectPrice = document.createElement("div"),
     projectButton = document.createElement("div");
   div.className =
-    "flex items-center justify-between flex px-[70px] py-[22px]  h-full";
-  projectEl.className = `w-[350px]`;
+    "flex items-center justify-between flex px-[20px] sm:px-[70px] py-[22px]  h-full";
+  projectEl.className = `w-[350px] text-[20px] sm:text-[16px]`;
   projectButton.className =
-    "btn  btn-shad text-center py-3 w-[250px] bg-[#00BB34] cursor-pointer inline-block text-white font-bold rounded-[10px]";
+    "btn  btn-shad text-center py-3 w-[250px] bg-[#00BB34] cursor-pointer hidden sm:inline-block text-white font-bold rounded-[10px]";
   projectPrice.className = "w-[350px] text-lg flex items-end gap-x-2";
   projectButton.textContent = `Заказать мастера`;
   projectEl.textContent = `${elem.text}`;
-  projectPrice.innerHTML = `Ремонт от <span class='text-3xl text-[#FF5C00] font-bold'>${elem.price} руб</span>`;
+  projectPrice.className = "pl-3 sm:pl-0 whitespace-nowrap ";
+  projectPrice.innerHTML = `Ремонт от <span class='text-[20px] sm:text-3xl text-[#FF5C00] font-bold'>${elem.price} руб</span>`;
   div.appendChild(projectEl);
   div.appendChild(projectPrice);
   div.appendChild(projectButton);
   priceTable.append(div);
 });
 const lastDiv = document.createElement("div");
-lastDiv.innerHTML = `<div class="px-[70px] py-[22px] font-normal text-lg italic underline">Подробные цены на ремонт стиральных машин</div>`;
+lastDiv.innerHTML = `<div class="px-[20px] sm:px-[70px] py-[22px] font-normal text-lg italic cursor-pointer underline">Подробные цены на ремонт стиральных машин</div>`;
 priceTable.append(lastDiv);
 
 // Render of list prices second
@@ -85,10 +86,10 @@ listInstruction.forEach((elem) => {
   imgs.src = `${elem.src}`;
   title.textContent = `${elem.text}`;
   parElem.className =
-    "py-5 relative w-[602px] rounded-[27px] flex items-center justify-center mb-11 bg-line";
+    "py-5 relative w-[306px] sm:w-[602px] rounded-[27px] flex items-center justify-center mb-11 bg-line";
   title.className = "text-white font-bold";
   child.className =
-    "bg-white rounded-[50%] h-[95px] w-[95px] border border-[#5F24A8] flex justify-center items-center absolute left-0";
+    "bg-white rounded-[50%] h-[61px] w-[61px] sm:w-[95px] sm:h-[95px] border border-[#5F24A8] flex justify-center items-center absolute left-0";
   child.append(imgs);
   parElem.append(child);
   parElem.append(title);
@@ -117,31 +118,34 @@ __btns.forEach((el) => {
 });
 const mediaQuery = window.matchMedia("(min-width: 768px)");
 
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  centeredSlides: false,
-
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+try {
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: false,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+} catch (e) {
+  document.querySelector(".swiper").style.display = "none";
+}
 //Push other data
 for (let i = 0; i < 4; i++) {
   slideUrls.push(...slideUrls);
   slideNames.push(...slideNames);
-  slideDates.push(...slideNames);
+  slideDates.push(...slideDates);
 }
 //render elem of slider
 
 for (let i = 0; i < 26; i++) {
   document.querySelector(".swiper-wrapper").innerHTML += `
-  <div class="swiper-slide bg-white rounded-[42px] px-7 py-6 " style="box-shadow: rgba(0, 0, 0, 0.1) 0px 3px 10px 5px; width: 236px; margin-right: 30px;" data-swiper-slide-index="2" role="group" aria-label="3 / 10"><div class="flex justify-between items-start mb-6">
+  <div class="swiper-slide  bg-white rounded-[42px] px-7 py-6 " style="box-shadow: rgba(0, 0, 0, 0.1) 0px 3px 10px 5px; width: 236px; margin-right: 30px;" data-swiper-slide-index="2" role="group" aria-label="3 / 10"><div class="flex justify-between items-start mb-6">
   <div class="flex items-center gap-x-4">
     <img src='${slideUrls[i]}' alt="" class="rounded-[50%]">
     <div>
@@ -183,3 +187,15 @@ __btnCalls.forEach((el) => {
     }, 15 * 60 * 1000);
   });
 });
+
+/*  Burger menu  */
+let isShow = false;
+burger.onclick = ({ target }) => {
+  isShow = !isShow;
+  isShow
+    ? (modalShow.style.display = "block")
+    : (modalShow.style.display = "none");
+  isShow
+    ? (target.src = "./img/cancel.svg")
+    : (target.src = "./img/list 1.svg");
+};
